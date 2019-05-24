e0d9f6e403574bcd2aaaa1c3d3b61cf27aab57a7
ericxcross <eric.cross@sympatico.ca>
2019-05-24 12:19:35 +0100

    printable CV and responsive CSS

--- js/app.js
+++ js/app.js
@@ -2,32 +2,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const readMoreButton = document.querySelector('#readmore');
   const aboutme = document.querySelector('#aboutme');
-  aboutme.style.display = "none";
-
-  readMoreButton.addEventListener("click", (evt)=>{
-    if (aboutme.style.display === "none") {
-      const readless = document.querySelector('#readless');
-      aboutme.style.display = "block";
-      readMoreButton.style.display = "none";
-      readless.addEventListener("click", (evt)=>{
-        aboutme.style.display = "none";
-        readMoreButton.style.display = "block";
-      })
-    }
-  })
+  if (aboutme !==null){
+    aboutme.style.display = "none";
+    readMoreButton.addEventListener("click", (evt)=>{
+      if (aboutme.style.display === "none") {
+        const readless = document.querySelector('#readless');
+        aboutme.style.display = "block";
+        readMoreButton.style.display = "none";
+        readless.addEventListener("click", (evt)=>{
+          aboutme.style.display = "none";
+          readMoreButton.style.display = "block";
+        })
+      }
+    })
+  }
 
   const scrollIcon = document.querySelector('#scroller');
-  window.onscroll = function() {
-  if (window.pageYOffset > 10) {
-    scrollIcon.classList.remove("load")
-  } else {
-    scrollIcon.classList.add("load")
+  if (scrollIcon !== null){
+    window.onscroll = function() {
+      if (window.pageYOffset > 10) {
+        scrollIcon.classList.remove("load")
+      } else {
+        scrollIcon.classList.add("load")
+      }
+    }
+    if (window.pageYOffset > 0){
+      scrollIcon.style.display = "none";
+    } else{
+      scrollIcon.style.display = "block";
+    };
   }
-}
-  if (window.pageYOffset > 0){
-    scrollIcon.style.display = "none";
-  } else{
-    scrollIcon.style.display = "block";
-  };
 
 })
