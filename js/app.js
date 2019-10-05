7df46e454f863e7acd4d3b09a74e360d9d9fdada
ericxcross <eric.cross@sympatico.ca>
2019-10-05 01:46:39 +0100

    font size and content chang

--- js/app.js
+++ js/app.js
@@ -1,36 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-  const readMoreButton = document.querySelector('#readmore');
-  const aboutme = document.querySelector('#aboutme');
-  if (aboutme !==null){
-    aboutme.style.display = "none";
-    readMoreButton.addEventListener("click", (evt)=>{
-      if (aboutme.style.display === "none") {
-        const readless = document.querySelector('#readless');
-        aboutme.style.display = "block";
-        readMoreButton.style.display = "none";
-        readless.addEventListener("click", (evt)=>{
-          aboutme.style.display = "none";
-          readMoreButton.style.display = "block";
-        })
-      }
-    })
-  }
-
-  const scrollIcon = document.querySelector('#scroller');
-  if (scrollIcon !== null){
-    window.onscroll = function() {
-      if (window.pageYOffset > 10) {
-        scrollIcon.classList.remove("load")
-      } else {
-        scrollIcon.classList.add("load")
-      }
-    }
-    if (window.pageYOffset > 0){
-      scrollIcon.style.display = "none";
-    } else{
-      scrollIcon.style.display = "block";
-    };
-  }
-
 })
+
+window.onload = function(){
+  document.querySelector(".fade-out").classList.remove("fade-out");
+}
