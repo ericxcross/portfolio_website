01cc1316dce89ddf42ee4cce9b93bd06fd9ffd6d
ericxcross <eric.cross@sympatico.ca>
2019-10-06 13:58:57 +0100

    update to images

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const headerText = document.querySelector('#info');
+  let opacity = 1
+  if (headerText !== null){
+    window.onscroll = function() {
+
+      opacity = window.pageYOffset - 150
+
+      if (opacity <= 0) {
+        headerText.style.opacity = '1';
+        headerText.style.display = 'block'
+      } else if (opacity > 0 && opacity <= 100){
+        headerText.style.opacity = 1 - (opacity/100);
+      } else {
+        headerText.style.opacity = '0';
+      }
+    }
+
+  }
 })
 
 window.onload = function(){
